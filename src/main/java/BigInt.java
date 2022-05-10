88da2f8698853c424fbe447d66e7c30612cd68ba
MAKUSref <tomaszmakowski002731@gmail.com>
2022-05-10 14:53:56 +0200

    BigInt: add, multiply, mod, pow

--- src/main/java/BigInt.java
+++ src/main/java/BigInt.java
@@ -58,37 +58,224 @@ public class BigInt implements Comparable<BigInt> {
 
   public void setValue(String num) {
     this.bigNumber = num.toCharArray();
-  } 
+  }
 
-  public void add(BigInt o) {
-    List<Character> sum = new ArrayList<>();
-    
-    int buffor = 0;
+  public String add(BigInt o) {
+    String str1 = this.getStringVal();
+    String str2 = o.getStringVal();
 
-    int i = this.length() - 1;
-    int j = o.length() - 1;
-    for (int k = 0; i >= 0 || j >= 0; i--, j--, k++) {
-      char firstCharNumber = i >= 0 ? this.bigNumber[i] : '0';
-      char secCharNumber = j >= 0 ? o.bigNumber[j] : '0';
-
-      int firstIntNumber = Character.getNumericValue(firstCharNumber);
-      int secIntNumber = Character.getNumericValue(secCharNumber); 
-
-      int sumResult = firstIntNumber + secIntNumber + buffor;
-      buffor = sumResult / 10;
-      char sumResultChar = Character.forDigit(sumResult % 10, 10);
-      sum.add(sumResultChar);
+    if (str1.length() > str2.length()){
+      String t = str1;
+      str1 = str2;
+      str2 = t;
     }
 
-    Collections.reverse(sum);
-    this.bigNumber = BigInt.listToCharsArr(sum);
+    // Take an empty String for storing result
+    String str = "";
+ 
+    // Calculate length of both String
+    int n1 = str1.length(), n2 = str2.length();
+ 
+    // Reverse both of Strings
+    str1=new StringBuilder(str1).reverse().toString();
+    str2=new StringBuilder(str2).reverse().toString();
+ 
+    int carry = 0;
+    for (int i = 0; i < n1; i++)
+    {
+      // Do school mathematics, compute sum of
+      // current digits and carry
+      int sum = ((int)(str1.charAt(i) - '0') +
+                  (int)(str2.charAt(i) - '0') + carry);
+      str += (char)(sum % 10 + '0');
+
+      // Calculate carry for next step
+      carry = sum / 10;
+    }
+ 
+    // Add remaining digits of larger number
+    for (int i = n1; i < n2; i++)
+    {
+      int sum = ((int)(str2.charAt(i) - '0') + carry);
+      str += (char)(sum % 10 + '0');
+      carry = sum / 10;
+    }
+ 
+    // Add remaining carry
+    if (carry > 0)
+      str += (char)(carry + '0');
+ 
+    // reverse resultant String
+    this.bigNumber = BigInt.reverseChars(str.toCharArray());
+ 
+    return this.getStringVal();
+  }
+
+  public String subtract(BigInt o) {
+    String str1 = this.getStringVal();
+    String str2 = o.getStringVal();
+
+    if (this.compareTo(o) == -1) {
+      String t = str1;
+      str1 = str2;
+      str2 = t;
+    }
+
+    String str = "";
+    int n1 = str1.length(), n2 = str2.length();
+
+    str1 = new StringBuilder(str1).reverse().toString();
+    str2 = new StringBuilder(str2).reverse().toString();
+
+    int carry = 0;
+
+    for (int i = 0; i < n2; i++) {
+      int sub = ((int)(str1.charAt(i) - '0') - (int)(str2.charAt(i) - '0') - carry);
+
+      if (sub < 0) {
+        sub = sub + 10;
+        carry = 1;
+      }
+      else
+        carry = 0;
+
+      str += (char)(sub + '0');
+    }
+
+    // subtract remaining digits of larger number
+    for (int i = n2; i < n1; i++) {
+      int sub = ((int)(str1.charAt(i) - '0') - carry);
+
+      // if the sub value is -ve, then make it
+      // positive
+      if (sub < 0) {
+        sub = sub + 10;
+        carry = 1;
+      }
+      else
+        carry = 0;
+
+      str += (char)(sub + '0');
+    }
+
+    char[] reverseChars = BigInt.reverseChars(str.toCharArray());
+    String correctNum = this.deleteZeros(String.valueOf(reverseChars));
+    this.bigNumber = correctNum.toCharArray();
+    return this.getStringVal();
+  }
+
+  public String multiply(BigInt o) {
+    String num1 = this.getStringVal();
+    String num2 = o.getStringVal();
+
+    int len1 = num1.length();
+    int len2 = num2.length();
+
+    if (len1 == 0 || len2 == 0)
+      return "0";
+
+    int result[] = new int[len1 + len2];
+
+    int i_n1 = 0;
+    int i_n2 = 0;
+
+    for (int i = len1 - 1; i >= 0; i--) {
+      int carry = 0;
+      int n1 = num1.charAt(i) - '0';
+
+      i_n2 = 0;
+
+      for (int j = len2 - 1; j >= 0; j--) {
+        int n2 = num2.charAt(j) - '0';
+        int sum = n1 * n2 + result[i_n1 + i_n2] + carry;
+
+        carry = sum / 10;
+
+        result[i_n1 + i_n2] = sum % 10;
+
+        i_n2++;
+      }
+
+      if (carry > 0)
+        result[i_n1 + i_n2] += carry;
+
+      i_n1++;
+    }
+
+    int i = result.length - 1;
+    while (i >= 0 && result[i] == 0)
+      i--;
+
+    if (i == -1)
+      return "0";
+
+    String s = "";
+
+    while (i >= 0)
+      s += (result[i--]);
+
+    this.bigNumber = s.toCharArray();
+    return s;
+  }
+
+  public String pow(BigInt pow) {
+    String num1 = this.getStringVal();
+    String num2 = pow.getStringVal();
+
+    BigInt newVal = new BigInt(num1);
+
+    BigInt i = new BigInt("1");
+    BigInt step = new BigInt("1");
+
+    for (; pow.compareTo(i) > 0; i.add(step)) {
+      newVal.multiply(this);
+    }
+
+    this.bigNumber = newVal.getValue();
+    return this.getStringVal();
+  }
+
+  public String mod(BigInt o) {
+    if (this.compareTo(o) == -1) {
+      return this.getStringVal();
+    } else if (this.compareTo(o) == 0) {
+      this.setValue("0");
+      return this.getStringVal();
+    }
+
+    BigInt copy = new BigInt(this.getStringVal());
+
+    while (copy.compareTo(o) >= 0) {
+      copy.subtract(o);
+    }
+
+    this.bigNumber = copy.getValue();
+    return this.getStringVal();
+  }
+
+  private String deleteZeros(String num) {
+    String newVal = "";
+    boolean zeros = true;
+    for (int i = 0; i < num.length(); i++) {
+      if (num.charAt(i) != '0') {
+        zeros = false;
+      }
+
+      if (!zeros) {
+        newVal += num.charAt(i);
+      } else if (zeros && i == num.length() - 1) {
+        newVal = "0";
+      }
+    }
+
+    return newVal;
   }
 
   @Override
   public int compareTo(BigInt o) {
     int firstLen = bigNumber.length;
     int secLen = o.getValue().length;
-    
+
     // sprawdzanie długości tablic
     if (firstLen > secLen) {
       return 1;
@@ -107,7 +294,7 @@ public class BigInt implements Comparable<BigInt> {
         return -1;
       }
     }
-    
+
     return 0;
   }
 }
--- src/test/java/BigIntTest.java
+++ src/test/java/BigIntTest.java
@@ -81,13 +81,77 @@ public class BigIntTest {
   public void addTest() {
     bigInt.setValue("123");
     BigInt bi = new BigInt("11");
-    bigInt.add(bi);
-    assertEquals("134", bigInt.getStringVal());
+    assertEquals("134", bigInt.add(bi));
 
     String expectedValue = "99247780936";
     bigInt.setValue("7778526");
     bi.setValue("99240002410");
-    bigInt.add(bi);
-    assertEquals(expectedValue, bigInt.getStringVal());
+    assertEquals(expectedValue, bigInt.add(bi));
+    
+
+    expectedValue = "10";
+    bigInt.setValue("9");
+    bi.setValue("1");
+    assertEquals(expectedValue, bigInt.add(bi));
+  }
+
+  @Test
+  public void subtractTest() {
+    String expectedValue = "100";
+    bigInt.setValue("1100");
+    BigInt bi = new BigInt("1000");
+
+    assertEquals(expectedValue, bigInt.subtract(bi));
+
+    expectedValue = "0";
+    bigInt.setValue("500");
+    bi.setValue("500");
+    assertEquals(expectedValue, bigInt.subtract(bi));
+
+    expectedValue = "1";
+    bigInt.setValue("2118187521397235888154583183918321221520083884298838480662479");
+    bi.setValue("2118187521397235888154583183918321221520083884298838480662480");
+    assertEquals(expectedValue, bigInt.subtract(bi));
+  }
+
+  @Test
+  public void multiplyTest() {
+    bigInt.setValue("1235421415454545454545454544");
+    BigInt bi = new BigInt("1714546546546545454544548544544545");
+    String expectedValue = "2118187521397235888154583183918321221520083884298838480662480";
+
+    assertEquals(expectedValue, bigInt.multiply(bi));
+  }
+
+  @Test
+  public void powTest() {
+    bigInt.setValue("2");
+    BigInt bi = new BigInt("100");
+    String expectedValue = "1267650600228229401496703205376";
+
+    assertEquals(expectedValue, bigInt.pow(bi));
+  }
+
+  @Test
+  public void modTest() {
+    bigInt.setValue("10");
+    BigInt bi = new BigInt("5");
+    String expectedValue = "0";
+    assertEquals(expectedValue, bigInt.mod(bi));
+
+    expectedValue = "10";
+    bigInt.setValue("10");
+    bi.setValue("11");
+    assertEquals(expectedValue, bigInt.mod(bi));
+
+    expectedValue = "10";
+    bigInt.setValue("10");
+    bi.setValue("2118187521397235888154583183918321221520083884298838480662480");
+    assertEquals(expectedValue, bigInt.mod(bi));
+
+    // expectedValue = "1";
+    bigInt.setValue("222222222222222222222222221516541354164321641654983516568459865649451654895649565"); 
+    bi.setValue("98989846546515649846548446549847984654654316546846468484984649849849849484649");
+    assertNotNull(bigInt.mod(bi));
   }
 }
