f21ad02bc2cd00ea4ff87262240adc4e95785831
Trung Anh Nguyen Nang <101046115+bakuganmw@users.noreply.github.com>
2022-05-11 22:00:08 +0200

    Update PrimeNumbers.java

--- src/main/java/PrimeNumbers.java
+++ src/main/java/PrimeNumbers.java
@@ -1,20 +1,27 @@
+package main;
+
 public class PrimeNumbers {
-  public static long getPrimeNumber(long limit) {
-    long num = limit;
-    while (!PrimeNumbers.isPrime(num)) {
-      num--;
+
+
+    public static boolean is_prime(BigInt n){
+        if (n.compareTo(new BigInt("2")) == 0 || n.compareTo(new BigInt("3")) == 0 ) {
+            return true;
+        }
+
+        if (n.compareTo(new BigInt("1")) == 0 || n.compareTo(new BigInt("1")) == -1 ||
+                n.mod(new BigInt("2")) == "0" || n.mod(new BigInt("3")) == "0") {
+            return false;
+        }
+        BigInt i = new BigInt("5");
+        BigInt six = new BigInt("6");
+        BigInt i2 = new BigInt("5");
+        i2.multiply(i);
+        for (; new BigInt(i2.getStringVal()).compareTo(n) == -1 ||new BigInt(i2.getStringVal()).compareTo(n) == 0; i.add(six) ){
+            if (n.mod(i) == "0" || n.mod(new BigInt(i.add(new BigInt("2")))) == "0") {
+                return false;
+            }
+        }
+
+        return true;
     }
-
-    return num;
-  }
-
-  public static boolean isPrime(long num) {
-    for (int i = 2; i < num / 2; i++) {
-      if (num % i == 0) {
-        return false;
-      }
-    }
-
-    return true;
-  }
 }
